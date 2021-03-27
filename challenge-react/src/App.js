c8f14c0cfdb440510a5a1a1f4cc3842da56cc0b4
four-leaf-4 <kojima.koike.lab@gmail.com>
2021-03-27 18:07:25 +0900

    context & hooks に置き換えた

--- challenge-react/src/App.js
+++ challenge-react/src/App.js
@@ -1,99 +1,82 @@
-import React, { Component } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import fetch from 'isomorphic-fetch';
 import styled from 'styled-components';
-import { connect } from 'react-redux';
-import { summaryDonations } from './helpers';
+
+import { StoreContext, AmountContext, CharitiesContext } from './index';
 
 const Card = styled.div`
   margin: 10px;
   border: 1px solid #ccc;
 `;
 
-export default connect((state) => state)(
-  class App extends Component {
-    state = {
-      charities: [],
-      selectedAmount: 10,
-    };
+const SectionCard = styled.div`
+  display: flex;
+`;
 
-    componentDidMount() {
-      const self = this;
-      fetch('http://localhost:3001/charities')
-        .then(function (resp) {
-          return resp.json();
-        })
-        .then(function (data) {
-          self.setState({ charities: data });
-        });
+const Message = styled.p`
+color: 'red',
+margin: '1em 0',
+fontWeight: 'bold',
+fontSize: '16px',
+textAlign: 'center',
+`;
 
-      fetch('http://localhost:3001/payments')
-        .then(function (resp) {
-          return resp.json();
-        })
-        .then(function (data) {
-          self.props.dispatch({
-            type: 'UPDATE_TOTAL_DONATE',
-            amount: summaryDonations(data.map((item) => item.amount)),
-          });
-        });
-    }
+const App = () => {
+  const { state } = useContext(StoreContext);
+  const { donate, message } = state;
 
-    render() {
-      const self = this;
-      const cards = this.state.charities.map(function (item, i) {
-        const payments = [10, 20, 50, 100, 500].map((amount, j) => (
-          <label key={j}>
-            <input
-              type="radio"
-              name="payment"
-              onClick={function () {
-                self.setState({ selectedAmount: amount });
-              }}
-            />
-            {amount}
-          </label>
-        ));
+  return (
+    <div>
+      <h1>Tamboon React</h1>
+      <p>All donations: {donate}</p>
+      <Message>{message}</Message>
+      <Cards />
+    </div>
+  );
+};
 
-        return (
-          <Card key={i}>
-            <p>{item.name}</p>
-            {payments}
-            <button
-              onClick={handlePay.call(
-                self,
-                item.id,
-                self.state.selectedAmount,
-                item.currency
-              )}
-            >
-              Pay
-            </button>
-          </Card>
-        );
-      });
+const Payments = () => {
+  const { selectedAmount, setSelectedAmount } = useContext(AmountContext);
+  console.log(selectedAmount);
+  const clickPayment = (e) => {
+    const { value } = e.target;
+    console.log(e.target.value);
+    setSelectedAmount({ selectedAmount: value });
+  };
 
-      const style = {
-        color: 'red',
-        margin: '1em 0',
-        fontWeight: 'bold',
-        fontSize: '16px',
-        textAlign: 'center',
-      };
+  return [10, 20, 50, 100, 500].map((amount, j) => (
+    <label key={j}>
+      <input
+        type="radio"
+        name="payment"
+        value={selectedAmount}
+        onClick={clickPayment}
+      />
+      {amount}
+    </label>
+  ));
+};
 
-      const donate = this.props.donate;
-      const message = this.props.message;
+const OneOfCard = ({ item }) => {
+  const { name } = item;
 
-      return (
-        <div>
-          <h1>Tamboon React</h1>
-          <p>All donations: {donate}</p>
-          <p style={style}>{message}</p>
-          {cards}
-        </div>
-      );
-    }
-  }
-);
+  return (
+    <Card>
+      <p>{name}</p>
+      <Payments />
+      <button onClick={handlePay}>Pay</button>
+    </Card>
+  );
+};
+
+const Cards = () => {
+  const { charities } = useContext(CharitiesContext);
+  const cards = charities.map((item, i) => <OneOfCard key={i} item={item} />);
+
+  return <SectionCard>{cards}</SectionCard>;
+};
+
+export default App;
 
 /**
  * Handle pay button
@@ -108,4 +91,7 @@ export default connect((state) => state)(
       body: `{ "charitiesId": ${id}, "amount": ${amount}, "currency": "${currency}" }`,
     })
  */
-function handlePay(id, amount, currency) {}
+
+const handlePay = (id, amount, currency) => {
+  console.log(id);
+};
--- challenge-react/src/index.js
+++ challenge-react/src/index.js
@@ -1,18 +1,18 @@
-import React from 'react';
+import React, { useReducer, useState, useEffect, useContext } from 'react';
 import { render } from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import App from './App';
+import { summaryDonations } from './helpers';
 
-const store = createStore(function (state, action) {
-  const _state =
-    state == null
-      ? {
-          donate: 0,
-          message: '',
-        }
-      : state;
+export const StoreContext = React.createContext();
 
+const initialState = {
+  donate: 0,
+  message: '',
+};
+
+const reducer = (_state, action) => {
   switch (action.type) {
     case 'UPDATE_TOTAL_DONATE':
       return Object.assign({}, _state, {
@@ -22,15 +22,75 @@ const store = createStore(function (state, action) {
       return Object.assign({}, _state, {
         message: action.message,
       });
-
     default:
       return _state;
   }
-});
+};
+
+export const StoreContextProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  useEffect(() => {
+    fetch('http://localhost:3001/payments')
+      .then(function (resp) {
+        return resp.json();
+      })
+      .then(function (data) {
+        console.log(data);
+        dispatch({
+          type: 'UPDATE_TOTAL_DONATE',
+          amount: summaryDonations(data.map((item) => item.amount)),
+        });
+      });
+  }, []);
+
+  return (
+    <StoreContext.Provider value={{ state, dispatch }}>
+      {children}
+    </StoreContext.Provider>
+  );
+};
+
+export const AmountContext = React.createContext();
+
+export const AmountContextProvider = ({ children }) => {
+  const [selectedAmount, setSelectedAmount] = useState(10);
+
+  return (
+    <AmountContext.Provider value={{ selectedAmount, setSelectedAmount }}>
+      {children}
+    </AmountContext.Provider>
+  );
+};
+
+export const CharitiesContext = React.createContext();
+
+export const CharitiesContextProvider = ({ children }) => {
+  const [charities, setCharities] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3001/charities')
+      .then(function (resp) {
+        return resp.json();
+      })
+      .then(function (data) {
+        setCharities(data);
+      });
+  }, []);
+
+  return (
+    <CharitiesContext.Provider value={{ charities }}>
+      {children}
+    </CharitiesContext.Provider>
+  );
+};
 
 render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <StoreContextProvider>
+    <AmountContextProvider>
+      <CharitiesContextProvider>
+        <App />
+      </CharitiesContextProvider>
+    </AmountContextProvider>
+  </StoreContextProvider>,
   document.getElementById('root')
 );
