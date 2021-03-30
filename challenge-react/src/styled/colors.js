563095144c7ac013fab650fe89923f87c8d0b0d8
four-leaf-4 <kojima.koike.lab@gmail.com>
2021-03-30 16:36:20 +0900

    Cardstyled CSSを編集

--- challenge-react/src/styled/colors.js
+++ challenge-react/src/styled/colors.js
@@ -1,4 +1,5 @@
 export const colors = {
   primary: 'palevioletred',
   secondary: '#0171bd',
+  lightgrey: '#999',
 };
--- challenge-react/src/styled/div.js
+++ challenge-react/src/styled/div.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { colors } from './colors';
 
 export const WrapperMessage = styled.div`
   position: absolute;
@@ -16,7 +17,9 @@ export const Cardstyled = styled.div`
   max-width: 400px;
   width: 100%;
   margin: 10px;
-  border: 1px solid #ccc;
+  padding: 0 0 1.5rem 0;
+  border-radius: 5px;
+  box-shadow: 1px 1px 8px ${colors.lightgrey};
 `;
 
 export const WrapperPaymentInputs = styled.div`
